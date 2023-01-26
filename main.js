4f62a30de710421c7f11677f81dbace6a9c5df3e
YaCaiBC <122326064+YaCaiBC@users.noreply.github.com>
2023-01-26 14:01:10 -0800

    HW4 second draft

--- main.js
+++ main.js
@@ -1,8 +1,60 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+// document.getElementById("show").addEventListener("click", function(){
 
-        document.getElementById("textbox1").value = "success";
-    });
+function getCheckedValue(allExercise) {
+    let radios =document.getElementById(allExercise);
+    for(i=0; i<radios.length; i++) {
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+    return null;
+}
 });
+
+exerArray =[];
+
+let ExerItem =function(pExerName, pMinutes, pCalories) {
+    this.exerName =pExerName;
+    this.minutes =parseInt(pMinutes);
+    this.calories =parseInt(pCalories);
+};
+
+exerArray.push(new ExerItem("Sit Ups", 10));
+exerArray.push(new ExerItem("Push Ups", 15));
+exerArray.push(new ExerItem("Jump Rope", 18));
+
+function add(){
+    let totalCalories =0;
+    let totalExerName =document.getElementById("exerName").value;
+    let totalExerMins =document.getElementById("minutes").value;
+    exerArray.push(new ExerItem(totalExerName,totalExerMins))
+
+    for(i =0; i<exerArray.length; i++) {
+        totalCalories =total*exerArray[i].minutes;
+    }
+    document.getElementById("totalMinutes").value =total;
+}
+
+function mostBurned() {
+    let winner =0;
+    for(i=0; i<exerArray.length; i++) {
+       if (exerArray[i].minutes>exerArray[winner].minutes)
+       {
+        winner =i;
+       }
+    }
+    document.getElementById("whichOutput").value =exerArray[winner].exerName;
+}
+
+function show(){
+    let message ="exercise minutes mostburned\n";
+       
+    for (i=0; i<exerArray.length;i++) {
+     message =message + PersonArray[i].Exercise +" "+ExerArray[i].minutes+" "+ExerArray[i].mostburned+"\n";
+     }
+     document.getElementById("output").value =message;
+     };
+
 
